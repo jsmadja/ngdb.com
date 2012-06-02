eaa0f0ec096d657f9f6de35344a6ba626d4dea64
jsmadja <jsmadja@financeactive.com>
2012-06-02 10:31:46 +0200

    ajout du tracking des utilisateurs

--- src/main/java/com/ngdb/web/components/Layout.java
+++ src/main/java/com/ngdb/web/components/Layout.java
@@ -1,5 +1,17 @@
 package com.ngdb.web.components;
 
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+import com.ngdb.entities.user.User;
+import com.ngdb.web.services.infrastructure.UserSession;
+
 public class Layout {
+
+	@Inject
+	private UserSession userSession;
+
+	public User getUser() {
+		return userSession.getUser();
+	}
 
 }
