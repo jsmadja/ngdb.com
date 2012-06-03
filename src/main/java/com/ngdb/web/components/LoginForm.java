193c82d7c74b7e5e9ec2b5d86c979ed5c72bc25b
jsmadja <jsmadja@financeactive.com>
2012-06-03 10:47:22 +0200

    deplacement du formulaire de login

--- src/main/java/com/ngdb/web/components/LoginForm.java
+++ src/main/java/com/ngdb/web/components/LoginForm.java
@@ -3,6 +3,7 @@ package com.ngdb.web.components;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.beaneditor.Validate;
 import org.apache.tapestry5.corelib.components.Form;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
@@ -13,9 +14,11 @@ import com.ngdb.web.services.infrastructure.UserSession;
 public class LoginForm {
 
 	@Property
+	@Validate("required")
 	private String login;
 
 	@Property
+	@Validate("required")
 	private String password;
 
 	@Inject
