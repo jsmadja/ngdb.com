07775b083d05e39e860094975cf12aee36c05528
jsmadja <jsmadja@financeactive.com>
2012-06-18 09:29:07 +0200

    cleaning GameView

--- src/main/java/com/ngdb/web/pages/article/game/GameView.java
+++ src/main/java/com/ngdb/web/pages/article/game/GameView.java
@@ -2,16 +2,12 @@ package com.ngdb.web.pages.article.game;
 
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.ioc.annotations.Inject;
-import org.hibernate.Session;
 
 import com.ngdb.entities.article.Game;
 import com.ngdb.entities.article.element.Note;
 import com.ngdb.entities.article.element.Review;
 import com.ngdb.entities.article.element.Tag;
 import com.ngdb.entities.reference.Genre;
-import com.ngdb.entities.user.User;
-import com.ngdb.web.services.infrastructure.CurrentUser;
 
 public class GameView {
 
@@ -36,18 +32,8 @@ public class GameView {
 	@Property
 	private Note note;
 
-	@Inject
-	private Session session;
-
-	@Property
-	private User user;
-
-	@Inject
-	private CurrentUser currentUser;
-
 	public void onActivate(Game game) {
 		this.game = game;
-		this.user = currentUser.getUser();
 	}
 
 	public Game onPassivate() {
