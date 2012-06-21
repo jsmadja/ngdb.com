bcacbf1bd3df9d21808c3afe5b377f4a54635ed1
jsmadja <jsmadja@financeactive.com>
2012-06-21 20:22:23 +0200

    tris des jeux

---
+++ src/main/java/com/ngdb/Comparators.java
@@ -0,0 +1,25 @@
+package com.ngdb;
+
+import java.util.Comparator;
+
+import com.ngdb.entities.article.Game;
+
+public class Comparators {
+
+	public static Comparator<Game> gamesByTitlePlatformOrigin = new Comparator<Game>() {
+		@Override
+		public int compare(Game g1, Game g2) {
+			if (g1.getTitle().compareToIgnoreCase(g2.getTitle()) != 0) {
+				return g1.getTitle().compareToIgnoreCase(g2.getTitle());
+			}
+			if (g1.getPlatform().compareTo(g2.getPlatform()) != 0) {
+				return g1.getPlatform().compareTo(g2.getPlatform());
+			}
+			if (g1.getOrigin().compareTo(g2.getOrigin()) != 0) {
+				return g1.getOrigin().compareTo(g2.getOrigin());
+			}
+			return 0;
+		}
+	};
+
+}
