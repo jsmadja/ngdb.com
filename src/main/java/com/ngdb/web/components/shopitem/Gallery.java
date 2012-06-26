5568acb71752d9167e71eeba981210ae49ecd63a
jsmadja <jsmadja@financeactive.com>
2012-06-26 18:43:37 +0200

    Ajout de la gallery pour les shopItems

---
+++ src/main/java/com/ngdb/web/components/shopitem/Gallery.java
@@ -0,0 +1,23 @@
+package com.ngdb.web.components.shopitem;
+
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+
+import com.ngdb.entities.article.element.Picture;
+import com.ngdb.entities.article.element.ShopItemPictures;
+import com.ngdb.entities.shop.ShopItem;
+
+public class Gallery {
+
+	@Property
+	@Parameter(allowNull = false)
+	private ShopItem shopItem;
+
+	@Property
+	private Picture picture;
+
+	public ShopItemPictures getPictures() {
+		return shopItem.getPictures();
+	}
+
+}
