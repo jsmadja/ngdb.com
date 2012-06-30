744cddf233371933db707410236d17fa4c6de03c
jsmadja <jsmadja@financeactive.com>
2012-06-30 19:02:40 +0200

    Correction graphique mineure sur GameView, le titre Review n est pas affiché si il n y a pas de review et que l utilisateur est anonyme

--- src/main/java/com/ngdb/web/pages/article/game/GameView.java
+++ src/main/java/com/ngdb/web/pages/article/game/GameView.java
@@ -4,12 +4,15 @@ import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
 
 import com.ngdb.entities.article.Game;
 import com.ngdb.entities.article.element.Note;
 import com.ngdb.entities.article.element.Tag;
 import com.ngdb.entities.reference.Genre;
+import com.ngdb.entities.user.User;
 import com.ngdb.web.Filter;
+import com.ngdb.web.services.infrastructure.CurrentUser;
 
 public class GameView {
 
@@ -30,6 +33,9 @@ public class GameView {
 
 	@Property
 	private Note note;
+
+	@Inject
+	private CurrentUser currentUser;
 
 	public void onActivate(Game game) {
 		this.game = game;
@@ -73,6 +79,10 @@ public class GameView {
 
 	public String getNgh() {
 		return StringUtils.isNumeric(game.getNgh()) ? "NGH " + game.getNgh() : game.getNgh();
+	}
+
+	public User getUser() {
+		return currentUser.getUser();
 	}
 
 }
