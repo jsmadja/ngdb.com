46177b0d8b2edb34e5224b19f8b3f53104ec2830
jsmadja <jsmadja@financeactive.com>
2012-07-08 11:14:54 +0200

    Suppression des pages games et hardwares

--- src/main/java/com/ngdb/web/pages/Museum.java
+++ src/main/java/com/ngdb/web/pages/Museum.java
@@ -2,13 +2,19 @@ package com.ngdb.web.pages;
 
 import static com.google.common.collect.Collections2.filter;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Lists;
@@ -16,11 +22,14 @@ import com.ngdb.Predicates;
 import com.ngdb.entities.GameFactory;
 import com.ngdb.entities.HardwareFactory;
 import com.ngdb.entities.article.Article;
+import com.ngdb.entities.article.Game;
+import com.ngdb.entities.article.element.Tag;
 import com.ngdb.entities.reference.Origin;
 import com.ngdb.entities.reference.Platform;
 import com.ngdb.entities.reference.Publisher;
 import com.ngdb.entities.reference.ReferenceService;
 import com.ngdb.entities.user.User;
+import com.ngdb.web.Filter;
 
 public class Museum {
 
@@ -35,6 +44,19 @@ public class Museum {
 
 	@Inject
 	private ReferenceService referenceService;
+
+	// ---- Parameters
+	@Persist
+	@Property
+	private Filter filter;
+
+	@Persist
+	@Property
+	private String filterValue;
+
+	@Persist
+	@Property
+	private Long id;
 
 	@Persist
 	@Property
@@ -66,18 +88,13 @@ public class Museum {
 	@Property
 	private boolean filteredByGames;
 
+	private static final Logger LOG = LoggerFactory.getLogger(Museum.class);
+
 	void onActivate() {
 		if (filterOrigin == null && filterPlatform == null) {
 			init();
 			this.user = null;
 		}
-	}
-
-	private void init() {
-		this.filterOrigin = referenceService.findOriginByTitle("Japan").getId();
-		this.filterPlatform = referenceService.findPlatformByName("Neo·Geo CD").getId();
-		this.filterPublisher = null;
-		this.filteredByGames = true;
 	}
 
 	boolean onActivate(User user) {
@@ -86,9 +103,45 @@ public class Museum {
 		return true;
 	}
 
+	boolean onActivate(String filter, String value) {
+		init();
+		this.filter = Filter.valueOf(Filter.class, filter);
+		this.filterValue = value;
+		if (StringUtils.isNumeric(value)) {
+			this.id = Long.valueOf(filterValue);
+			switch (this.filter) {
+			case byOrigin:
+				filterOrigin = id;
+				break;
+			case byPlatform:
+				filterPlatform = id;
+				break;
+			case byPublisher:
+				filterPublisher = id;
+				break;
+			}
+		}
+		return true;
+	}
+
+	private void init() {
+		this.filterOrigin = referenceService.findOriginByTitle("Japan").getId();
+		this.filterPlatform = referenceService.findPlatformByName("Neo·Geo CD").getId();
+		this.filterPublisher = null;
+		this.filteredByGames = true;
+		this.filterValue = null;
+		this.filter = Filter.none;
+	}
+
 	public List<Article> getArticles() {
-		Collection<Article> filteredArticles;
 		List<Predicate<Article>> filters = Lists.newArrayList();
+		applyExternalParameters(filters);
+		Collection<Article> filteredArticles = applyFilters(filters);
+		return new ArrayList<Article>(filteredArticles);
+	}
+
+	private Collection<Article> applyFilters(List<Predicate<Article>> filters) {
+		Collection<Article> filteredArticles;
 		if (filteredByGames) {
 			filteredArticles = new ArrayList<Article>(gameFactory.findAll());
 		} else {
@@ -101,7 +154,7 @@ public class Museum {
 			filters.add(new Predicates.PlatformPredicate(currentPlatform()));
 		}
 		if (filterPublisher != null) {
-			filters.add(new Predicates.PublisherPredicate(referenceService.findPublisherBy(filterPublisher)));
+			filters.add(new Predicates.PublisherPredicate(currentPublisher()));
 		}
 		for (Predicate<Article> filter : filters) {
 			filteredArticles = filter(filteredArticles, filter);
@@ -109,7 +162,56 @@ public class Museum {
 		if (user != null) {
 			filteredArticles = filter(filteredArticles, keepOnlyOwnedArticlesBy(user));
 		}
-		return new ArrayList<Article>(filteredArticles);
+		return filteredArticles;
+	}
+
+	private void applyExternalParameters(List<Predicate<Article>> filters) {
+		switch (filter) {
+		case byNgh:
+			filters.add(new Predicate<Article>() {
+				@Override
+				public boolean apply(Article input) {
+					if (input.getType().equals(Game.class)) {
+						Game game = (Game) input;
+						String ngh = game.getNgh();
+						if (ngh != null) {
+							return ngh.equalsIgnoreCase(filterValue);
+						}
+					}
+					return false;
+				}
+			});
+			break;
+		case byOrigin:
+			filterOrigin = id;
+			break;
+		case byPlatform:
+			filterPlatform = id;
+			break;
+		case byPublisher:
+			filterPublisher = id;
+			break;
+		case byReleaseDate:
+			final Date releaseDate = toReleaseDate();
+			filters.add(new Predicate<Article>() {
+				@Override
+				public boolean apply(Article input) {
+					Date inputReleaseDate = input.getReleaseDate();
+					return inputReleaseDate != null && inputReleaseDate.equals(releaseDate);
+				}
+			});
+			break;
+		case byTag:
+			final Tag tag = referenceService.findTagById(id);
+			this.filterValue = tag.getName();
+			filters.add(new Predicate<Article>() {
+				@Override
+				public boolean apply(Article input) {
+					return input.containsTag(tag);
+				}
+			});
+			break;
+		}
 	}
 
 	private Predicate<Article> keepOnlyOwnedArticlesBy(final User user) {
@@ -248,17 +350,31 @@ public class Museum {
 			queryLabel += " from " + origin.getTitle();
 		}
 		if (filterPublisher != null) {
-			Publisher publisher = referenceService.findPublisherBy(filterPublisher);
+			Publisher publisher = currentPublisher();
 			queryLabel += " published by " + publisher.getName();
 		}
 		if (filterPlatform != null) {
 			Platform platform = referenceService.findPlatformById(filterPlatform);
 			queryLabel += " on " + platform.getName();
 		}
-		/*
-		 * if (filter != null) { switch (filter) { case byNgh: queryLabel += " with ngh '" + filterValue + "'"; break; case byReleaseDate: queryLabel += " with release date '" + new SimpleDateFormat("MM/dd/yyyy").format(toReleaseDate()) + "'"; break; case byTag: queryLabel += " with tag '" + filterValue + "'"; break; } }
-		 */
+		if (filter != null) {
+			switch (filter) {
+			case byNgh:
+				queryLabel += " with ngh '" + filterValue + "'";
+				break;
+			case byReleaseDate:
+				queryLabel += " with release date '" + new SimpleDateFormat("MM/dd/yyyy").format(toReleaseDate()) + "'";
+				break;
+			case byTag:
+				queryLabel += " with tag '" + filterValue + "'";
+				break;
+			}
+		}
 		return queryLabel;
+	}
+
+	private Publisher currentPublisher() {
+		return referenceService.findPublisherBy(filterPublisher);
 	}
 
 	public int getNumResults() {
@@ -275,6 +391,15 @@ public class Museum {
 
 	public boolean isArticleInThisPublisher() {
 		return getNumArticlesInThisPublisher() > 0;
+	}
+
+	private Date toReleaseDate() {
+		try {
+			return new SimpleDateFormat("yyyy-MM-dd").parse(filterValue);
+		} catch (ParseException e) {
+			LOG.error("Wrong date: " + filterValue, e);
+			return new Date();
+		}
 	}
 
 }
--- src/test/java/com/ngdb/web/pages/MuseumTest.java
+++ src/test/java/com/ngdb/web/pages/MuseumTest.java
@@ -17,61 +17,61 @@ import com.ngdb.entities.reference.ReferenceService;
 import com.ngdb.web.Filter;
 
 @RunWith(MockitoJUnitRunner.class)
-public class GamesTest {
+public class MuseumTest {
 
 	// (all games from Japan, published by Snk on NeoGeoCD)
 
 	@InjectMocks
-	private Games games;
+	private Museum museum;
 
 	@Mock
 	private ReferenceService referenceService;
 
 	@Test
 	public void should_create_empty_query_label() {
-		assertThat(games.getQueryLabel()).isEqualTo("all games");
+		assertThat(museum.getQueryLabel()).isEqualTo("all games");
 	}
 
 	@Test
 	public void should_create_query_label_with_origin() {
-		ReflectionTestUtils.setField(games, "filterOrigin", 0L);
+		ReflectionTestUtils.setField(museum, "filterOrigin", 0L);
 		Mockito.when(referenceService.findOriginById(Mockito.anyLong())).thenReturn(new Origin("Japan"));
-		assertThat(games.getQueryLabel()).isEqualTo("all games from Japan");
+		assertThat(museum.getQueryLabel()).isEqualTo("all games from Japan");
 	}
 
 	@Test
 	public void should_create_query_label_with_publisher() {
-		ReflectionTestUtils.setField(games, "filterPublisher", 0L);
+		ReflectionTestUtils.setField(museum, "filterPublisher", 0L);
 		Mockito.when(referenceService.findPublisherBy(Mockito.anyLong())).thenReturn(new Publisher("SNK"));
-		assertThat(games.getQueryLabel()).isEqualTo("all games published by SNK");
+		assertThat(museum.getQueryLabel()).isEqualTo("all games published by SNK");
 	}
 
 	@Test
 	public void should_create_query_label_with_platform() {
-		ReflectionTestUtils.setField(games, "filterPlatform", 0L);
+		ReflectionTestUtils.setField(museum, "filterPlatform", 0L);
 		Mockito.when(referenceService.findPlatformById(Mockito.anyLong())).thenReturn(new Platform("NeoGeoCD"));
-		assertThat(games.getQueryLabel()).isEqualTo("all games on NeoGeoCD");
+		assertThat(museum.getQueryLabel()).isEqualTo("all games on NeoGeoCD");
 	}
 
 	@Test
 	public void should_create_query_label_with_ngh() {
-		ReflectionTestUtils.setField(games, "filterValue", "nghX");
-		ReflectionTestUtils.setField(games, "filter", Filter.byNgh);
-		assertThat(games.getQueryLabel()).isEqualTo("all games with ngh 'nghX'");
+		ReflectionTestUtils.setField(museum, "filterValue", "nghX");
+		ReflectionTestUtils.setField(museum, "filter", Filter.byNgh);
+		assertThat(museum.getQueryLabel()).isEqualTo("all games with ngh 'nghX'");
 	}
 
 	@Test
 	public void should_create_query_label_with_release_date() {
-		ReflectionTestUtils.setField(games, "filterValue", "1995-12-19");
-		ReflectionTestUtils.setField(games, "filter", Filter.byReleaseDate);
-		assertThat(games.getQueryLabel()).isEqualTo("all games with release date '12/19/1995'");
+		ReflectionTestUtils.setField(museum, "filterValue", "1995-12-19");
+		ReflectionTestUtils.setField(museum, "filter", Filter.byReleaseDate);
+		assertThat(museum.getQueryLabel()).isEqualTo("all games with release date '12/19/1995'");
 	}
 
 	@Test
 	public void should_create_query_label_with_tag() {
-		ReflectionTestUtils.setField(games, "filterValue", "tagX");
-		ReflectionTestUtils.setField(games, "filter", Filter.byTag);
-		assertThat(games.getQueryLabel()).isEqualTo("all games with tag 'tagX'");
+		ReflectionTestUtils.setField(museum, "filterValue", "tagX");
+		ReflectionTestUtils.setField(museum, "filter", Filter.byTag);
+		assertThat(museum.getQueryLabel()).isEqualTo("all games with tag 'tagX'");
 	}
 
 }
