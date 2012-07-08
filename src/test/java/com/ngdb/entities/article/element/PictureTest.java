43ec91244b48ba9297967481223c7aea380013b8
jsmadja <jsmadja@financeactive.com>
2012-07-08 22:11:29 +0200

    Reparation test

--- src/test/java/com/ngdb/entities/article/element/PictureTest.java
+++ src/test/java/com/ngdb/entities/article/element/PictureTest.java
@@ -8,7 +8,7 @@ public class PictureTest {
 	@Test
 	public void should_translated_in_watermarked_url() {
 		Picture picture = new Picture("02590a2-e5bf-4637-a42b-3deab74e2ed0.jpg");
-		Assert.assertEquals("02590a2-e5bf-4637-a42b-3deab74e2ed0_wm.jpg", picture.toWatermarkedUrl());
+		Assert.assertEquals("02590a2-e5bf-4637-a42b-3deab74e2ed0_high.jpg", picture.toWatermarkedUrl());
 	}
 
 }
