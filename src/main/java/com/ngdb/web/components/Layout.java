28857befded0aae3e9679b88ed00f263dc525758
Julien Smadja <jsmadja@xebia.fr>
2012-07-30 09:18:48 +0200

    Reagencement de la page Market

--- src/main/java/com/ngdb/web/components/Layout.java
+++ src/main/java/com/ngdb/web/components/Layout.java
@@ -1,5 +1,8 @@
 package com.ngdb.web.components;
 
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
 
@@ -8,11 +11,22 @@ import com.ngdb.web.services.infrastructure.CurrentUser;
 
 public class Layout {
 
+    @Property
+    @Parameter
+    private Boolean showLeftContent;
+
 	@Inject
 	private CurrentUser userSession;
 
 	@Inject
 	private Request request;
+
+    @SetupRender
+    public void init() {
+        if(showLeftContent == null) {
+            showLeftContent = true;
+        }
+    }
 
 	public User getUser() {
 		return userSession.getUser();
