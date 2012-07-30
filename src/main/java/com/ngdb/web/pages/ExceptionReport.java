c55eb81d406a8562c3cc57caff8a271743a3ca04
Julien Smadja <jsmadja@xebia.fr>
2012-07-30 19:40:31 +0200

    Ajout d un workaround pour le probleme du form dans un form

--- src/main/java/com/ngdb/web/pages/ExceptionReport.java
+++ src/main/java/com/ngdb/web/pages/ExceptionReport.java
@@ -1,14 +1,26 @@
 package com.ngdb.web.pages;
 
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Environment;
 import org.apache.tapestry5.services.ExceptionReporter;
+import org.apache.tapestry5.services.FormSupport;
 
 public class ExceptionReport implements ExceptionReporter {
 
-	@Property
-	private Throwable exception;
+    @Property
+    private Throwable exception;
 
-	public void reportException(Throwable exception) {
-		this.exception = exception;
-	}
+    @Inject
+    private Environment environment;
+
+    void setupRender() {
+        if (environment.peek(FormSupport.class) != null) {
+            environment.pop(FormSupport.class);
+        }
+    }
+
+    public void reportException(Throwable exception) {
+        this.exception = exception;
+    }
 }
