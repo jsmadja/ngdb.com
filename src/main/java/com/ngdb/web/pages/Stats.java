0857353cc66ff4b4834e7f9b2ba9ed47bd51f01d
Julien Smadja <jsmadja@xebia.fr>
2012-08-07 09:15:15 +0200

    Ajout du most owned article

--- src/main/java/com/ngdb/web/pages/Stats.java
+++ src/main/java/com/ngdb/web/pages/Stats.java
@@ -1,11 +1,18 @@
 package com.ngdb.web.pages;
 
+import com.ngdb.entities.ArticleFactory;
+import com.ngdb.entities.article.Article;
+import com.ngdb.entities.user.CollectionObject;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 import com.ngdb.entities.Population;
 import com.ngdb.entities.WishBox;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+
+import java.math.BigInteger;
 
 public class Stats {
 
@@ -18,6 +25,9 @@ public class Stats {
 	@Property
 	private Long soldCount;
 
+    @Property
+    private Article mostOwnedArticle;
+
 	@Inject
 	private com.ngdb.entities.Market market;
 
@@ -27,11 +37,24 @@ public class Stats {
 	@Inject
 	private WishBox wishBox;
 
-	@SetupRender
+    @Inject
+    private Session session;
+
+    @Inject
+    private ArticleFactory articleFactory;
+
+    @Property
+    private Long mostOwnedArticleCount;
+
+    @SetupRender
 	public void init() {
 		this.userCount = population.getNumUsers();
 		this.wishListCount = wishBox.getNumWishes();
 		this.soldCount = market.getNumSoldItems();
-	}
+
+        Object[] result = (Object[]) session.createSQLQuery("SELECT article_id,COUNT(*) FROM CollectionObject GROUP BY article_id ORDER BY COUNT(*) DESC").setMaxResults(1).list().get(0);
+        this.mostOwnedArticle =  articleFactory.findById(((BigInteger)result[0]).longValue());
+	    this.mostOwnedArticleCount = ((BigInteger)result[1]).longValue();
+    }
 
 }
