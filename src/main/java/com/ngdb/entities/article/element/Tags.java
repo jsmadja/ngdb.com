51ff1adda879133653a06eebb302d9bea0c94360
Julien Smadja <jsmadja@xebia.fr>
2012-08-24 09:47:43 +0200

    fix Les tags ne sont pas triés par ordre alpha

--- src/main/java/com/ngdb/entities/article/element/Tags.java
+++ src/main/java/com/ngdb/entities/article/element/Tags.java
@@ -1,15 +1,16 @@
 package com.ngdb.entities.article.element;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
 import javax.persistence.Embeddable;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OrderBy;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
@@ -18,34 +19,35 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Tags implements Iterable<Tag> {
 
-	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(name = "ArticleTags", inverseJoinColumns = { @JoinColumn(name = "tag_id") }, joinColumns = { @JoinColumn(name = "article_id") })
-	private Set<Tag> tags = new HashSet<Tag>();
+    @OrderBy("name")
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "ArticleTags", inverseJoinColumns = { @JoinColumn(name = "tag_id") }, joinColumns = { @JoinColumn(name = "article_id") })
+    private Set<Tag> tags = new TreeSet<Tag>();
 
-	public Set<Tag> all() {
-		return Collections.unmodifiableSet(tags);
-	}
+    public Set<Tag> all() {
+        return Collections.unmodifiableSet(tags);
+    }
 
-	@Override
-	public Iterator<Tag> iterator() {
-		return tags.iterator();
-	}
+    @Override
+    public Iterator<Tag> iterator() {
+        return tags.iterator();
+    }
 
-	public boolean contains(String tagName) {
-		for (Tag tag : tags) {
-			if (tag.hasName(tagName)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public boolean contains(String tagName) {
+        for (Tag tag : tags) {
+            if (tag.hasName(tagName)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public void add(Tag tag) {
-		tags.add(tag);
-	}
+    public void add(Tag tag) {
+        tags.add(tag);
+    }
 
-	public boolean contains(Tag tag) {
-		return tags.contains(tag);
-	}
+    public boolean contains(Tag tag) {
+        return tags.contains(tag);
+    }
 
 }
