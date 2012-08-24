c13c8c9c70feaf469f26c553b4cf0aac63abf24b
Julien Smadja <jsmadja@xebia.fr>
2012-08-24 13:15:11 +0200

    Somthing Wrong ? > Rajouter ID

--- src/main/java/com/ngdb/web/pages/Contact.java
+++ src/main/java/com/ngdb/web/pages/Contact.java
@@ -4,43 +4,79 @@ import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.beaneditor.Validate;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.ngdb.entities.Population;
 import com.ngdb.web.services.MailService;
+import com.ngdb.web.services.infrastructure.CurrentUser;
 
 public class Contact {
 
-	@Property
-	@Validate("required")
-	private String title;
+    @Property
+    @Validate("required")
+    private String title;
 
-	@Property
-	@Validate("required")
-	private String comment;
+    @Property
+    @Validate("required")
+    private String comment;
 
-	@Persist
-	@Property
-	private String message;
+    @Property
+    @Validate("required")
+    private String from;
 
-	@Inject
-	private MailService mailService;
+    @Persist
+    @Property
+    private String message;
 
-	@Inject
-	private Population population;
+    @Persist
+    @Property
+    private Long id;
 
-	public void onActivate() {
+    @Inject
+    private MailService mailService;
 
-	}
+    @Inject
+    private Population population;
 
-	public boolean onActivate(String title) {
-		this.title = title;
-		return true;
-	}
+    @Inject
+    private CurrentUser currentUser;
 
-	public Object onSuccess() {
-		mailService.sendMail("julien.smadja+neogeodb@gmail.com", "[CONTACT] " + comment, title);
-		message = "Your comment has been successfully sent";
-		return this;
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(Contact.class);
+
+    public void onActivate() {
+
+    }
+
+    public boolean onActivate(String title) {
+        this.title = title;
+        return true;
+    }
+
+    public boolean onActivate(String title, Long id) {
+        this.title = title;
+        this.id = id;
+        return true;
+    }
+
+    public Object onSuccess() {
+        String body = "[CONTACT] " + comment + " - ";
+        if (id != null) {
+            body += " article #" + id + ", ";
+        }
+        if (currentUser.isAnonymous()) {
+            body += " from " + from;
+        } else {
+            body += " from " + currentUser.getUsername() + " (" + currentUser.getUser().getEmail() + ")";
+        }
+        LOG.info("Sending feedback : \n" + body);
+        mailService.sendMail("anzymus@neogeodb.com", body, title);
+        message = "Your comment has been successfully sent";
+        return this;
+    }
+
+    public boolean isAnonymous() {
+        return currentUser.isAnonymous();
+    }
 
 }
