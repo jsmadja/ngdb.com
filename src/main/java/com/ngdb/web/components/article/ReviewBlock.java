c6c4c9561dd9be8d6a6f7f4fbefcf88d82417969
Julien Smadja <jsmadja@xebia.fr>
2012-08-25 20:11:27 +0200

    Suppression erreur dans les logs a cause du OnSuccess de ReviewBlock

--- src/main/java/com/ngdb/web/components/article/ReviewBlock.java
+++ src/main/java/com/ngdb/web/components/article/ReviewBlock.java
@@ -22,79 +22,78 @@ import com.ngdb.web.services.infrastructure.CurrentUser;
 
 public class ReviewBlock {
 
-	@Property
-	@Parameter
-	private Article article;
+    @Property
+    @Parameter
+    private Article article;
 
-	@Property
-	private Review review;
+    @Property
+    private Review review;
 
-	@Inject
-	private CurrentUser currentUser;
+    @Inject
+    private CurrentUser currentUser;
 
-	@Inject
-	private GameFactory gameFactory;
+    @Inject
+    private GameFactory gameFactory;
 
-	private Collection<String> suggestions = new TreeSet<String>();
+    private Collection<String> suggestions = new TreeSet<String>();
 
-	@Property
-	private String label;
+    @Property
+    private String label;
 
-	@Property
-	private String url;
+    @Property
+    private String url;
 
-	@Property
-	private String mark;
+    @Property
+    private String mark;
 
-	@Inject
-	private Registry registry;
+    @Inject
+    private Registry registry;
 
-	void onActivate() {
-		suggestions.addAll(registry.findAllTags());
-	}
+    void onActivate() {
+        suggestions.addAll(registry.findAllTags());
+    }
 
-	@CommitAfter
-	public Object onSuccess() {
-		if (isNotBlank(label) && isNotBlank(url) && isNotBlank(mark)) {
-			article.updateModificationDate();
-			currentUser.addReviewOn(article, label, url, mark);
-		}
-		return this;
-	}
+    @CommitAfter
+    public void onSuccess() {
+        if (isNotBlank(label) && isNotBlank(url) && isNotBlank(mark)) {
+            article.updateModificationDate();
+            currentUser.addReviewOn(article, label, url, mark);
+        }
+    }
 
-	public User getUser() {
-		return currentUser.getUser();
-	}
+    public User getUser() {
+        return currentUser.getUser();
+    }
 
-	public Set<Review> getReviews() {
-		if (article instanceof Game) {
-			Game game = (Game) article;
-			Set<Review> reviews = new TreeSet<Review>(game.getReviews().all());
-			List<Game> relatedGames = gameFactory.findAllByNgh(game.getNgh());
-			for (Game relatedGame : relatedGames) {
-				reviews.addAll(relatedGame.getReviews().all());
-			}
-			return reviews;
-		}
-		return article.getReviews().all();
-	}
+    public Set<Review> getReviews() {
+        if (article instanceof Game) {
+            Game game = (Game) article;
+            Set<Review> reviews = new TreeSet<Review>(game.getReviews().all());
+            List<Game> relatedGames = gameFactory.findAllByNgh(game.getNgh());
+            for (Game relatedGame : relatedGames) {
+                reviews.addAll(relatedGame.getReviews().all());
+            }
+            return reviews;
+        }
+        return article.getReviews().all();
+    }
 
-	public String getStars() {
-		String mark = review.getMark();
-		int numStars = Integer.valueOf("" + mark.charAt(0));
-		boolean halfStar = "5".equals("" + mark.charAt(1));
-		String stars = "";
-		int numGreyStars = 5 - numStars;
-		for (int i = 0; i < numStars; i++) {
-			stars += "<img width=\"20px\" src=\"/img/stars/star.png\">";
-		}
-		if (halfStar) {
-			stars += "<img width=\"20px\" src=\"/img/stars/half_star.png\">";
-			numGreyStars--;
-		}
-		for (int i = 0; i < numGreyStars; i++) {
-			stars += "<img width=\"20px\" src=\"/img/stars/grey_star.png\">";
-		}
-		return stars;
-	}
+    public String getStars() {
+        String mark = review.getMark();
+        int numStars = Integer.valueOf("" + mark.charAt(0));
+        boolean halfStar = "5".equals("" + mark.charAt(1));
+        String stars = "";
+        int numGreyStars = 5 - numStars;
+        for (int i = 0; i < numStars; i++) {
+            stars += "<img width=\"20px\" src=\"/img/stars/star.png\">";
+        }
+        if (halfStar) {
+            stars += "<img width=\"20px\" src=\"/img/stars/half_star.png\">";
+            numGreyStars--;
+        }
+        for (int i = 0; i < numGreyStars; i++) {
+            stars += "<img width=\"20px\" src=\"/img/stars/grey_star.png\">";
+        }
+        return stars;
+    }
 }
