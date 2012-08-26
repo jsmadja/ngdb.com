6c3c243d8faf142a2808ab1e1a63422789af2d95
Julien Smadja <jsmadja@xebia.fr>
2012-08-26 10:08:16 +0200

    Tri des articles du market par ordre alpha

--- src/main/java/com/ngdb/entities/shop/ShopItem.java
+++ src/main/java/com/ngdb/entities/shop/ShopItem.java
@@ -10,7 +10,6 @@ import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -42,171 +41,171 @@ public class ShopItem implements Comparable<ShopItem> {
 
     private static final int MAX_DETAIL_LENGTH = 1024;
 
-	@Column(name = "creation_date", nullable = false)
-	private Date creationDate;
+    @Column(name = "creation_date", nullable = false)
+    private Date creationDate;
 
-	@Column(name = "modification_date", nullable = false)
-	private Date modificationDate;
+    @Column(name = "modification_date", nullable = false)
+    private Date modificationDate;
 
-	@Embedded
-	private ShopItemPictures pictures = new ShopItemPictures();
+    @Embedded
+    private ShopItemPictures pictures = new ShopItemPictures();
 
-	@OneToOne
-	private State state;
+    @OneToOne
+    private State state;
 
-	@ManyToOne(optional = false, fetch = LAZY)
-	private Article article;
+    @ManyToOne(optional = false, fetch = LAZY)
+    private Article article;
 
-	private String details;
+    private String details;
 
-	private Boolean sold = false;
+    private Boolean sold = false;
 
-	private Double priceInDollars;
+    private Double priceInDollars;
 
-	private Double priceInEuros;
+    private Double priceInEuros;
 
-	@Embedded
-	private PotentialBuyers potentialBuyers;
+    @Embedded
+    private PotentialBuyers potentialBuyers;
 
-	public ShopItem() {
-		this.creationDate = this.modificationDate = new Date();
-	}
+    public ShopItem() {
+        this.creationDate = this.modificationDate = new Date();
+    }
 
-	@PreUpdate
-	public void update() {
-		this.modificationDate = new Date();
-	}
+    @PreUpdate
+    public void update() {
+        this.modificationDate = new Date();
+    }
 
-	public String getTitle() {
-		return article.getTitle();
-	}
+    public String getTitle() {
+        return article.getTitle();
+    }
 
-	public State getState() {
-		return state;
-	}
+    public State getState() {
+        return state;
+    }
 
-	public Article getArticle() {
-		return article;
-	}
+    public Article getArticle() {
+        return article;
+    }
 
-	public String getDetails() {
-		return details;
-	}
+    public String getDetails() {
+        return details;
+    }
 
-	public Date getCreationDate() {
-		return creationDate;
-	}
+    public Date getCreationDate() {
+        return creationDate;
+    }
 
-	public User getSeller() {
-		return seller;
-	}
+    public User getSeller() {
+        return seller;
+    }
 
-	public boolean isSold() {
-		return sold == null ? false : sold;
-	}
+    public boolean isSold() {
+        return sold == null ? false : sold;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public Date getModificationDate() {
-		return modificationDate;
-	}
+    public Date getModificationDate() {
+        return modificationDate;
+    }
 
-	public void setState(State state) {
-		this.state = state;
-	}
+    public void setState(State state) {
+        this.state = state;
+    }
 
-	public void setDetails(String details) {
-		details = StringUtils.defaultString(details);
-		int end = details.length() < MAX_DETAIL_LENGTH ? details.length() : MAX_DETAIL_LENGTH;
-		this.details = details.substring(0, end);
-	}
+    public void setDetails(String details) {
+        details = StringUtils.defaultString(details);
+        int end = details.length() < MAX_DETAIL_LENGTH ? details.length() : MAX_DETAIL_LENGTH;
+        this.details = details.substring(0, end);
+    }
 
-	public void setCreationDate(Date creationDate) {
-		this.creationDate = creationDate;
-	}
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
 
-	public void setArticle(Article article) {
-		this.article = article;
-	}
+    public void setArticle(Article article) {
+        this.article = article;
+    }
 
-	public void setSeller(User seller) {
-		this.seller = seller;
-	}
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
 
-	public void addPicture(Picture picture) {
-		pictures.add(picture);
-	}
+    public void addPicture(Picture picture) {
+        pictures.add(picture);
+    }
 
-	public ShopItemPictures getPictures() {
-		return pictures;
-	}
+    public ShopItemPictures getPictures() {
+        return pictures;
+    }
 
-	public Picture getMainPicture() {
-		if (hasNoPicture()) {
-			return article.getPictures().first();
-		}
-		return pictures.first();
-	}
+    public Picture getMainPicture() {
+        if (hasNoPicture()) {
+            return article.getPictures().first();
+        }
+        return pictures.first();
+    }
 
-	public void addPotentialBuyer(User potentialBuyer) {
-		potentialBuyers.add(potentialBuyer);
-	}
+    public void addPotentialBuyer(User potentialBuyer) {
+        potentialBuyers.add(potentialBuyer);
+    }
 
-	public boolean isNotAlreadyWantedBy(User potentialBuyer) {
-		return !potentialBuyers.contains(potentialBuyer);
-	}
+    public boolean isNotAlreadyWantedBy(User potentialBuyer) {
+        return !potentialBuyers.contains(potentialBuyer);
+    }
 
-	public void sold() {
-		this.sold = true;
-	}
+    public void sold() {
+        this.sold = true;
+    }
 
-	public boolean hasNoPicture() {
-		return pictures.first().equals(Picture.EMPTY);
-	}
+    public boolean hasNoPicture() {
+        return pictures.first().equals(Picture.EMPTY);
+    }
 
-	public void updateModificationDate() {
-		this.modificationDate = new Date();
-	}
+    public void updateModificationDate() {
+        this.modificationDate = new Date();
+    }
 
-	@Override
-	public String toString() {
-		return format("{0} by {1} for ${2} ({3}e)", getArticle().getTitle(), seller.getLogin(), getPriceInDollars(), getPriceInEuros());
-	}
+    @Override
+    public String toString() {
+        return format("{0} by {1} for ${2} ({3}e)", getArticle().getTitle(), seller.getLogin(), getPriceInDollars(), getPriceInEuros());
+    }
 
-	public String getForSaleDate() {
-		return new PrettyTime(Locale.UK).format(getCreationDate());
-	}
+    public String getForSaleDate() {
+        return new PrettyTime(Locale.UK).format(getCreationDate());
+    }
 
-	public void setPriceInDollars(Double priceInDollars) {
-		this.priceInDollars = priceInDollars;
-	}
+    public void setPriceInDollars(Double priceInDollars) {
+        this.priceInDollars = priceInDollars;
+    }
 
-	public Double getPriceInDollars() {
-		return priceInDollars;
-	}
+    public Double getPriceInDollars() {
+        return priceInDollars;
+    }
 
-	public void setPriceInEuros(Double priceInEuros) {
-		this.priceInEuros = priceInEuros;
-	}
+    public void setPriceInEuros(Double priceInEuros) {
+        this.priceInEuros = priceInEuros;
+    }
 
-	public Double getPriceInEuros() {
-		return priceInEuros;
-	}
+    public Double getPriceInEuros() {
+        return priceInEuros;
+    }
 
-	public void removePicture(Picture picture) {
-		pictures.remove(picture);
-	}
+    public void removePicture(Picture picture) {
+        pictures.remove(picture);
+    }
 
-	@Override
-	public int compareTo(ShopItem shopItem) {
-		return article.compareTo(shopItem.article);
-	}
+    @Override
+    public int compareTo(ShopItem shopItem) {
+        return article.getTitle().compareToIgnoreCase(shopItem.article.getTitle());
+    }
 
-	public Set<User> getPotentialBuyers() {
-		return potentialBuyers.all();
-	}
+    public Set<User> getPotentialBuyers() {
+        return potentialBuyers.all();
+    }
 
     public int getPicturesCount() {
         return pictures.getCount();
