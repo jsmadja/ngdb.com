78c95b565788a60bf51e96b824cf5e69b11e057b
Julien Smadja <jsmadja@xebia.fr>
2012-08-28 21:14:43 +0200

    reparation test

--- src/main/java/com/ngdb/entities/article/Article.java
+++ src/main/java/com/ngdb/entities/article/Article.java
@@ -99,7 +99,7 @@ public abstract class Article implements Comparable<Article>, Serializable {
 
     @Embedded
     @XmlTransient
-    private Reviews reviews;
+    private Reviews reviews = new Reviews();
 
     @Embedded
     @XmlTransient
