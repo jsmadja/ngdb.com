43a9a3ddea34d4b9f47c63ed53606f1f3f79c52c
Julien Smadja <julien.smadja@gmail.com>
2012-08-29 21:48:58 +0200

    correction script supprimer_images_inutilises.sh

--- src/main/script/supprimer_images_inutilises.sh
+++ src/main/script/supprimer_images_inutilises.sh
@@ -1,24 +1,13 @@
+#!/bin/bash
 for line in $(file -i `find /ngdb/images/articles -type f` | grep -v "medium\|small\|high" | cut -d':' -f1) 
 do
-	echo "SELECT * FROM Picture WHERE url ='$line';" > zop.sql
-	a=$(mysql --user=ngdb --password=ngdb ngdb < zop.sql | wc -l)
-	if [ $a -eq 0 ] 
+	echo "SELECT * FROM Picture WHERE url ='$line';" > /tmp/zop.sql
+	a=$(mysql --user=ngdb --password=ngdb ngdb < /tmp/zop.sql | wc -l)
+	if [ $a -eq 0 ]
 	then
-		article_id=$(echo "$line" | cut -d'/' -f5)
-		article_dir="/ngdb/images/articles/$article_id"
-		#rm -rf $article_dir
-	fi
-done
-
-for line in $(file -i `find /ngdb/images/shop-items -type f` | grep -v "medium\|small\|high" | cut -d':' -f1)
-do
-        echo "SELECT * FROM Picture WHERE url ='$line';" > zop.sql
-        a=$(mysql --user=ngdb --password=ngdb ngdb < zop.sql | wc -l)
-        if [ $a -eq 0 ]
-        then
-                article_id=$(echo "$line" | cut -d'/' -f5)
-                article_dir="/ngdb/images/shop-items/$article_id"
-                echo $article_dir
-		#rm -rf $article_dir
-        fi 
+		article_id=$(echo $line | cut -d'/' -f5)
+		image_id=$(echo $line | cut -d'/' -f6 | cut -d'.' -f1)
+		rm -rfv /ngdb/images/articles/$article_id/$image_id*
+		echo $line
+	fi 
 done
