2d24144124a4ff62ed706d02831a015b64fd1610
Julien Smadja <julien.smadja@gmail.com>
2012-09-01 09:37:23 +0200

    ajout script backup

---
+++ src/main/script/backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+home=/home/neogeodb-admin
+home_script=$home/src/main/script
+dir=/tmp/ngdb_backup
+filename=`date "+%Y_%m_%d"`
+
+rm -rf $dir/*
+mkdir -p $dir/sql
+
+echo "sql backup ..."
+mysqldump -uroot -pm1710calos --opt ngdb > $dir/sql/$filename.sql
+
+echo "resources backup ..."
+cp -rf /ngdb $dir/ngdb
+cd $dir
+find . -name "*_wm*" -type d -exec rm -v {} \;
+
+echo "compressing files ..."
+tar -zcf $filename.tar.gz *
+
+rm -rf sql
+rm -rf ngdb
+
+echo "preparing cadaver input ..."
+sed s/__filename__/`date "+%Y_%m_%d"`.tar.gz/ <$home_script/cadaver.script > $home_script/cadaver.script.seded
+
+echo "uploading backup file to dumptruck ..."
+cadaver -t https://dav.dumptruck.goldenfrog.com/r/giganews.com < $home_script/cadaver.script.seded
+
+echo "cleaning temp directory ..."
+rm -rf sql
+rm -rf ngdb
+rm -rf $home_script/cadaver.script.seded
+
+echo "backup done!"
