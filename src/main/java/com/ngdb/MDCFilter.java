61f5c2d1ff483d76d4a9292dfce9b5a509be61ce
Julien Smadja <jsmadja@xebia.fr>
2012-09-28 22:54:47 +0200

    ajout des utilisateurs connus dans le MDCFilter

--- src/main/java/com/ngdb/MDCFilter.java
+++ src/main/java/com/ngdb/MDCFilter.java
@@ -6,6 +6,8 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.apache.shiro.subject.support.DefaultSubjectContext.PRINCIPALS_SESSION_KEY;
 
@@ -22,14 +24,25 @@ public class MDCFilter implements Filter {
         chain.doFilter(request, response);
     }
 
+    private static final Map<String, String> knownUsers = new HashMap<String, String>() {{
+        put("173.192.34.91", "monitis-us");
+        put("37.59.48.24", "monitis-fr");
+        put("127.0.0.1", "local");
+    }};
+
     private String getUserPrincipal(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
+        String ip = request.getRemoteAddr();
         if (session == null) {
-            return request.getRemoteAddr();
+            String knownUser = knownUsers.get(ip);
+            if(knownUser != null) {
+                return knownUser;
+            }
+            return ip;
         }
         Object login = session.getAttribute(PRINCIPALS_SESSION_KEY);
         if(login == null) {
-            return request.getRemoteAddr();
+            return ip;
         }
         return login.toString();
     }
