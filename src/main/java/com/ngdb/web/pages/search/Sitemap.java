87fade2442ce68fb631aa59187650003c155cb81
Julien Smadja <jsmadja@xebia.fr>
2012-10-09 21:26:26 +0200

    refactoring url sitemap

--- src/main/java/com/ngdb/web/pages/search/Sitemap.java
+++ src/main/java/com/ngdb/web/pages/search/Sitemap.java
@@ -27,11 +27,13 @@ public class Sitemap {
                 "      xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"\n" +
                 "      xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
                 "      xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9\n" +
-                "            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">\n" +
-                "\n");
+                "            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">\n");
         List<Article> articles = session.createCriteria(Article.class).list();
         for (Article article : articles) {
             String url = host+article.getViewPage()+"/"+article.getId()+"/"+article.getSuffix();
+            url = url.replaceAll("gameView", "view");
+            url = url.replaceAll("hardwareView", "view");
+            url = url.replaceAll("accessoryView", "view");
             String s = "<url>\n" +
                     "  <loc>"+url+"</loc>\n" +
                     "  <changefreq>daily</changefreq>\n" +
