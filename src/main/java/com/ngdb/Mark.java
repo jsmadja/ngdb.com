36c7d0f5014e91ad0e6e8176c2af43a26a285952
Julien Smadja <jsmadja@xebia.fr>
2012-10-12 18:07:20 +0200

    Changement algorithme calcul des etoiles

---
+++ src/main/java/com/ngdb/Mark.java
@@ -0,0 +1,84 @@
+package com.ngdb;
+
+import static org.apache.commons.lang.StringUtils.remove;
+
+public class Mark {
+
+    private double starsAsDouble;
+    private String nativeValue;
+
+    public Mark(String nativeValue) {
+        this.nativeValue = nativeValue;
+        if(nativeValue.contains("%")) {
+            starsAsDouble = convertPercents();
+        } else if (nativeValue.contains("/")) {
+            starsAsDouble = convertQuotients();
+        }
+    }
+
+    private double convertQuotients() {
+        String sup = nativeValue.split("/")[0];
+        sup = sup.replace(',','.');
+        Double supAsDouble = Double.valueOf(sup);
+
+        double valueFrom0To5;
+        int inf = Integer.valueOf(nativeValue.split("/")[1]);
+        if(inf == 5) {
+            valueFrom0To5 = supAsDouble;
+        } else {
+            valueFrom0To5 = (supAsDouble/inf)*5;
+        }
+        return round(valueFrom0To5);
+    }
+
+    private double convertPercents() {
+        double value = Double.parseDouble(nativeValue.replaceAll("%", ""));
+        value -= 50;
+        if(value < 0) {
+            value = 0;
+        }
+        double valueFrom0To5 = value / 10;
+        return round(valueFrom0To5);
+    }
+
+    private Double round(Double value) {
+        Double v = new Double(value.intValue());
+        double diff = value - v;
+        if (diff < 0.5) {
+            value = v;
+        } else if (diff >= 0.5) {
+            value = v + 0.5;
+        }
+        return value;
+    }
+
+    public double getStarsAsDouble() {
+        return starsAsDouble;
+    }
+
+    public String toString() {
+        if(starsAsDouble == 0) {
+            return "00";
+        }
+        int i = (int) (starsAsDouble * 10);
+        if(i < 10) {
+            return "0"+i;
+        }
+        return i +"";
+    }
+
+    public int getAsPercent() {
+        if (nativeValue == null) {
+            return 0;
+        }
+        if (nativeValue.contains("%")) {
+            return Integer.valueOf(remove(nativeValue, '%'));
+        } else if (nativeValue.contains("/")) {
+            Double numerator = Double.valueOf(nativeValue.split("/")[0]);
+            Double denominator = Double.valueOf(nativeValue.split("/")[1]);
+            return (int) ((numerator / denominator) * 100);
+        }
+        return 0;
+    }
+
+}
--- src/main/java/com/ngdb/entities/article/element/Review.java
+++ src/main/java/com/ngdb/entities/article/element/Review.java
@@ -1,5 +1,6 @@
 package com.ngdb.entities.article.element;
 
+import com.ngdb.Mark;
 import com.ngdb.entities.AbstractEntity;
 import com.ngdb.entities.article.Article;
 import org.hibernate.annotations.Cache;
@@ -48,67 +49,11 @@ public class Review extends AbstractEntity implements Comparable<Review> {
     }
 
     public String getMark() {
-        if (mark == null) {
-            return "00";
-        }
-        if (mark.contains("%")) {
-            return convertPercents();
-        } else if (mark.contains("/")) {
-            return convertQuotient();
-        }
-        return "00";
+        return new Mark(mark).toString();
     }
 
     public int getMarkInPercent() {
-        if (mark == null) {
-            return 0;
-        }
-        if (mark.contains("%")) {
-            return Integer.valueOf(remove(mark, '%'));
-        } else if (mark.contains("/")) {
-            Double numerator = Double.valueOf(mark.split("/")[0]);
-            Double denominator = Double.valueOf(mark.split("/")[1]);
-            return (int) ((numerator / denominator) * 100);
-        }
-        return 0;
-    }
-
-    private String convertQuotient() {
-        double inf = Double.valueOf(mark.split("/")[1]);
-        if (inf == 0) {
-            return "00";
-        }
-        double sup = Double.valueOf(mark.split("/")[0]);
-        Double i = new BigDecimal((sup * 10) / inf).setScale(0, HALF_UP).doubleValue() / 2;
-        return i.toString().replace(".", "").substring(0, 2);
-    }
-
-    private String convertPercents() {
-        Double value = Double.parseDouble(mark.replaceAll("%", "")) / 2;
-
-        value = round(value);
-
-        String string = Integer.toString(value.intValue());
-        if (string.equals("0")) {
-            string = "00";
-        }
-        return string;
-    }
-
-    private Double round(Double value) {
-        value = value / 10;
-        Double v = new Double(value.intValue());
-        double diff = value - v;
-        if (diff < 0.25) {
-            value = v;
-        } else if (diff >= 0.75) {
-            value = v + 1;
-        } else if (diff >= 0.25 && diff < 0.5) {
-            value = v;
-        } else if (diff >= 0.5 && diff <= 0.74) {
-            value = v + 0.5;
-        }
-        return value * 10;
+        return new Mark(mark).getAsPercent();
     }
 
     public String getUrl() {
