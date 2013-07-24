0ed1b7dc6b929dbdccea0eb5da9a34977c69b84f
Julien Smadja <julien.smadja@gmail.com>
2013-07-24 09:00:45 +0200

    Reduce available currencies

--- src/main/java/com/ngdb/web/services/infrastructure/CurrencyService.java
+++ src/main/java/com/ngdb/web/services/infrastructure/CurrencyService.java
@@ -1,15 +1,12 @@
 package com.ngdb.web.services.infrastructure;
 
-import com.google.common.base.Function;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 
-import javax.annotation.Nullable;
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
-import static com.google.common.collect.Collections2.transform;
 import static java.math.RoundingMode.HALF_DOWN;
 
 public class CurrencyService {
@@ -17,14 +14,7 @@ public class CurrencyService {
     private ConversionRateService conversionRateService = new ConversionRateService();
 
     public Collection<String> allCurrencies() {
-        List<CurrencyUnit> registeredCurrencies = CurrencyUnit.registeredCurrencies();
-        return transform(registeredCurrencies, new Function<CurrencyUnit, String>() {
-            @Override
-            @Nullable
-            public String apply(@Nullable CurrencyUnit input) {
-                return input.getCurrencyCode();
-            }
-        });
+        return Arrays.asList("EUR", "USD", "GBP", "JPY", "CHF");
     }
 
     public Double fromToRate(Double price, String from, String to) {
